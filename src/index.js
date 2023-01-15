c4372087182760f916664ebafdbd848b33ed767f
LPandorf <leticiapandorf@gmail.com>
2023-01-15 19:43:55 -0300

    teste 14

--- src/index.js
+++ src/index.js
@@ -23,13 +23,13 @@ mongoClient.connect().then(()=>{
     name: joi.string().min(1).required(),
     laststatus: joi.number()
 }); */
-const messageFormat=joi.object({
+/* const messageFormat=joi.object({
     //from: joi.string().required(),
     to: joi.string().min(1).required(),
     text: joi.string().min(1).required(),
     type: joi.string().valid("message", "private_message").required(),
     time: joi.string()
-});
+}); */
 
 // /participants
 app.post("/participants", async (req, res) => {
@@ -125,7 +125,8 @@ app.get("/participants", async (req, res) => {
 // /messages
 app.post("/messages", async (req, res) => {
     const serverMessage = req.body;
-    const { user } = req.headers;
+    //const { user } = req.headers;
+    const user = req.headers.user;
 
     try {
         const validation = messageFormat.validate(serverMessage, { abortEarly: false });
@@ -133,7 +134,7 @@ app.post("/messages", async (req, res) => {
         const message = {
             from: user,
             ...serverMessage,
-            time: dayjs().format("HH:MM:SS")
+            time: dayjs().format("HH:mm:ss")
         };
 
 
